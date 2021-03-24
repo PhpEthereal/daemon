9f5b7eb7a08a3df4cf81bc6f2fd2278ad0c6d2a6
Paul Water <p.r.water@setbased.nl>
2021-03-24 10:57:04 +0100

    Renamed namespace.

--- src/DaemonContext.php
+++ src/DaemonContext.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Ethereal\Daemon;
+namespace Plaisio\Daemon;
 
 /**
  * Context for turning the current program into a daemon process.
--- src/DaemonException.php
+++ src/DaemonException.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Ethereal\Daemon;
+namespace Plaisio\Daemon;
 
 use SetBased\Exception\RuntimeException;
 
